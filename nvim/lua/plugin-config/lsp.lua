c1bf20096333fba95d36272ed2f238eb7eb06752
caojoshua <cao.joshua@yahoo.com>
2021-04-18 13:38:28 -0700

    Assign high priority to LSP signs.

--- nvim/lua/plugin-config/lsp.lua
+++ nvim/lua/plugin-config/lsp.lua
@@ -31,6 +31,9 @@ local lsp_attach = function(client, bufnr)
   vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
     vim.lsp.diagnostic.on_publish_diagnostics, {
       virtual_text = false,
+      signs = {
+        priority = 1000
+      }
     }
   )
 
