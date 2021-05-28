701e43cfad25e8541c25f2a0236905c84f5ab5aa
caojoshua <cao.joshua@yahoo.com>
2021-05-28 00:28:58 -0700

    lf file manager

--- install.sh
+++ install.sh
@@ -12,3 +12,4 @@ ln -s ${dir}/alacritty.yml ~/.config/alacritty/alacritty.yml
 ln -s ${dir}/zsh/.zprofile ~/.zprofile
 ln -s ${dir}/zsh/.zshrc ~/.zshrc
 ln -s ${dir}/.gitconfig ~/.gitconfig
+ln -s ${dir}/lfrc ~/.config/lf/lfrc
