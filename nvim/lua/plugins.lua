4b5674ef5451b7d2d09e24e37005599ee7c0f4f6
Joshua Cao <cao.joshua@yahoo.com>
2023-11-12 00:31:40 -0800

    nvim switch from packer to lazy package manager

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -1,90 +1,72 @@
--- install packer if it hasn't been installed yet
-local install_path = vim.fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
-if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
-  vim.fn.system({'git', 'clone', 'https://github.com/wbthomason/packer.nvim', install_path})
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
 end
+vim.opt.rtp:prepend(lazypath)
 
--- auto compile
-vim.cmd('autocmd BufWritePost plugins.lua PackerCompile')
-
-local packer = require('packer')
-
-packer.init({
-  -- compile_path seems to be broken now
-  -- compile_path = vim.fn.stdpath('data'),
-  opt_default = false,
-  git = {
-    depth = 500,
-  },
-})
-
--- load plugins
-packer.startup(function(use)
-  -- packer can manage itself
-  use { 'wbthomason/packer.nvim', opt = false }
-
+require("lazy").setup({
   -- libraries, depedencies for other plugins
-  use { 'nvim-lua/popup.nvim' }
-  use { 'nvim-lua/plenary.nvim' }
-
+  'nvim-lua/popup.nvim',
+  'nvim-lua/plenary.nvim',
   -- LSP
-  use { 'williamboman/mason.nvim' }
-  use { 'williamboman/mason-lspconfig.nvim' }
-  use { 'neovim/nvim-lspconfig' }
-  use { 'ray-x/lsp_signature.nvim' }
-  use { 'simrat39/symbols-outline.nvim' }
-
+  'williamboman/mason.nvim',
+  'williamboman/mason-lspconfig.nvim',
+  'neovim/nvim-lspconfig',
+  'ray-x/lsp_signature.nvim',
+  'simrat39/symbols-outline.nvim',
   -- language specific
-  use { 'mfussenegger/nvim-jdtls' }
-  use { 'folke/neodev.nvim' }
-
+  'mfussenegger/nvim-jdtls',
+  'folke/neodev.nvim',
   -- completion
-  use { 'hrsh7th/cmp-buffer'}
-  use { 'saadparwaiz1/cmp_luasnip' }
-  use { 'hrsh7th/cmp-nvim-lua' }
-  use { 'hrsh7th/cmp-nvim-lsp' }
-  use { 'hrsh7th/cmp-path' }
-  use { 'hrsh7th/nvim-cmp'}
-
+  'hrsh7th/cmp-buffer',
+  'saadparwaiz1/cmp_luasnip',
+  'hrsh7th/cmp-nvim-lua',
+  'hrsh7th/cmp-nvim-lsp',
+  'hrsh7th/cmp-path',
+  'hrsh7th/nvim-cmp',
   -- snippets
-  use { 'rafamadriz/friendly-snippets' } -- snippets collection
-  use { 'L3MON4D3/LuaSnip' } -- snippets engine
-
+  'rafamadriz/friendly-snippets', -- snippets collection
+  'L3MON4D3/LuaSnip',             -- snippets engine
   -- treesitter
-  use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
-  use { 'nvim-treesitter/nvim-treesitter-textobjects' }
-  use { 'nvim-treesitter/playground' }
-  use { 'p00f/nvim-ts-rainbow' }
-
+  {
+    'nvim-treesitter/nvim-treesitter',
+    build = ':TSUpdate'
+  },
+  'nvim-treesitter/nvim-treesitter-textobjects',
+  'nvim-treesitter/playground',
+  'p00f/nvim-ts-rainbow',
   -- debugging
-  use { 'mfussenegger/nvim-dap' }
-  use { 'rcarriga/nvim-dap-ui' }
-
+  'mfussenegger/nvim-dap',
+  'rcarriga/nvim-dap-ui',
   -- telescope
-  use { 'nvim-telescope/telescope.nvim' }
-  use { 'nvim-telescope/telescope-fzf-native.nvim', run = 'make' }
-
+  'nvim-telescope/telescope.nvim',
+  {
+    'nvim-telescope/telescope-fzf-native.nvim',
+    build = 'make',
+  },
   -- file explorer
-  use { 'ptzz/lf.vim' }
-  use { 'voldikss/vim-floaterm' }
-
+  'ptzz/lf.vim',
+  'voldikss/vim-floaterm',
   -- git
-  use { 'airblade/vim-gitgutter' }
-  use { 'tpope/vim-fugitive' }
-
+  'airblade/vim-gitgutter',
+  'tpope/vim-fugitive',
   -- colorscheme
-  use { 'RRethy/nvim-base16' }
-
+  'RRethy/nvim-base16',
   -- text objects/motions
-  use { 'phaazon/hop.nvim' }
-  use { 'tpope/vim-commentary' }
-  use { 'tpope/vim-surround' }
-
+  'phaazon/hop.nvim',
+  'tpope/vim-commentary',
+  'tpope/vim-surround',
   -- misc
-  use { 'lukas-reineke/indent-blankline.nvim' }
-  use { 'ntpeters/vim-better-whitespace' }
-  use { 'rmagatti/auto-session' }
-  use { 'tpope/vim-sleuth' }
-  use { 'christoomey/vim-tmux-navigator' }
-end
-)
+  'lukas-reineke/indent-blankline.nvim',
+  'ntpeters/vim-better-whitespace',
+  'rmagatti/auto-session',
+  'tpope/vim-sleuth',
+  'christoomey/vim-tmux-navigator',
+})
